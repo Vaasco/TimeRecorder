fc4775822aa6e7c20c1d072fe978abe7fa5831a9
Vaasco <vascobranco13@gmail.com>
2021-04-09 13:32:52 +0100

    atualização -> Colocação do break

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,7 +7,17 @@ object HAL { }
 fun isBit(mask:Int):Boolean = UsbPort.`in`().inv().and(mask) == mask
 
 
-//fun readBits(mask:Int):Int{
+fun readBits(mask:Int):Int{
+     val a = UsbPort.`in`().inv().and(mask)
+     var idx:Int = 0
+     var nMask = mask
+     for (i in 0..7){
+         if(nMask % 2 != 0) idx = i; break
+         nMask = nMask shr 1
+     }
+    return a shr idx
+
+}
 
 
 
@@ -17,15 +27,7 @@ fun isBit(mask:Int):Boolean = UsbPort.`in`().inv().and(mask) == mask
     fun main(args: Array<String>) {
         UsbPort.`in`()
         Time.sleep(5000)
-        println(isBit(1))
+        println(readBits(12))
         Time.sleep(5000)
-        println(isBit(1))
-        Time.sleep(5000)
-        println(isBit(1))
-        Time.sleep(5000)
-        println(isBit(1))
-        Time.sleep(5000)
-        println(isBit(1))
-        Time.sleep(5000)
-        println(isBit(1))
+        println(readBits(12))
     }
