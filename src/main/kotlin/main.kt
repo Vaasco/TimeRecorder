3b2ef04cfcf17e9279366054c75329227922cdc5
costalapada <franciscocosta.py@gmail.com>
2021-04-09 13:45:39 +0100

    Correção da Função readBits() da classe HAL

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,32 +2,33 @@ import isel.leic.UsbPort
 import isel.leic.utils.Time
 
 
-object HAL { }
+object HAL {
 
 fun isBit(mask:Int):Boolean = UsbPort.`in`().inv().and(mask) == mask
 
-
 fun readBits(mask:Int):Int{
-     val a = UsbPort.`in`().inv().and(mask)
-     var idx:Int = 0
-     var nMask = mask
-     for (i in 0..7){
-         if(nMask % 2 != 0) idx = i; break
-         nMask = nMask shr 1
-     }
+    val a = UsbPort.`in`().inv().and(mask)
+    var idx:Int = 0
+    var nMask = mask
+    for (i in 0..7){
+        if(nMask % 2 != 0){
+            idx = i
+            break
+        }
+        nMask = nMask shr 1
+    }
     return a shr idx
-
 }
 
-
-
-
-
-
-    fun main(args: Array<String>) {
-        UsbPort.`in`()
-        Time.sleep(5000)
-        println(readBits(12))
-        Time.sleep(5000)
-        println(readBits(12))
-    }
+}
+fun main(args: Array<String>) {
+    UsbPort.`in`()
+    Time.sleep(5000)
+    println(HAL.readBits(0b00001100))
+    Time.sleep(5000)
+    println(HAL.readBits(0b00001100))
+    Time.sleep(5000)
+    println(HAL.readBits(0b00001100))
+    Time.sleep(5000)
+    println(HAL.readBits(0b00001100))
+}
