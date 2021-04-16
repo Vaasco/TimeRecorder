cdbb837bef1c15fad66bff95ded5b871c2f64e8c
Teo <teo_pienescu@hotmail.com>
2021-04-16 11:47:40 +0100

    Final KBD and HAL

--- src/main/kotlin/KBD.kt
+++ src/main/kotlin/KBD.kt
@@ -1,17 +1,29 @@
 import isel.leic.UsbPort
+import isel.leic.utils.Time.getTimeInMillis
 
 object KBD {
-    const val NONE = 0
+    const val NONE: Char = 0.toChar()
+    const val KEY_MASK = 0b00011110
+    const val D_VAL = 0b00000001
     val array = charArrayOf('1','4','7','*','2','5','8','0','3','6','9','#')
 
-    fun getKey(): Char =
-
-        if (HAL.isBit(0b00000001))
-            array[HAL.readBits(0b00011110)]
+    fun getKey(): Char {
+        val value = HAL.readBits(KEY_MASK)
+        return if (HAL.isBit(D_VAL) && value in array.indices)
+            array[value]
         else
-            NONE.toChar()
+            NONE
+    }
 
-   // fun waitKey
-
-
+    fun waitKey(timeout: Long): Char{
+        val msBefore = getTimeInMillis()
+        var currentTime = 0L
+        while(currentTime - msBefore < timeout){
+            val value = HAL.readBits(KEY_MASK)
+            val valid = HAL.isBit(D_VAL)
+            if(valid && value in array.indices) return array[value]
+            currentTime = getTimeInMillis()
+        }
+        return NONE
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,13 +3,5 @@ import isel.leic.utils.Time
 
 fun main(args: Array<String>) {
     UsbPort.`in`()
-    KBD.getKey()
-    Time.sleep(5000)
-    KBD.getKey()
-    Time.sleep(5000)
-    KBD.getKey()
-    Time.sleep(5000)
-    KBD.getKey()
-    Time.sleep(5000)
-    KBD.getKey()
+    println(KBD.waitKey(10000))
 }
