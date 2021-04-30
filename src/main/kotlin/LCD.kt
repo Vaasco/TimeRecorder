54eb05ba17b40bff88d109499ebfe0072867608b
Teo <teo_pienescu@hotmail.com>
2021-04-30 11:54:54 +0100

    LCD 2.0

--- src/main/kotlin/LCD.kt
+++ src/main/kotlin/LCD.kt
@@ -2,25 +2,36 @@ import isel.leic.UsbPort
 import isel.leic.utils.Time
 
 object LCD{ // Escreve no LCD usando a interface a 4 bits
-
+    private const val DATA_MASK = 0b00001111
+    private const val RS_MASK = 0b00010000
+    private const val ENABLE_MASK = 0b00100000
     private const val LINES = 2
     private const val COLS = 16 // Dimensão do display.
 
     // Escreve um nibble de comando/dados no LCD
     fun writeNibble(rs: Boolean, data: Int){
-        if (rs) HAL.setBits(0b00010000) // Rs High
-        else HAL.clearBits(0b00010000) // Rs Low
-        HAL.setBits(0b00100000) // Enable High
-        HAL.writeBits(0b00001111, data)
-        HAL.clearBits(0b00100000) // Enable Low
+        if (rs) HAL.setBits(RS_MASK) // Rs High
+        else HAL.clearBits(RS_MASK) // Rs Low
+        HAL.setBits(ENABLE_MASK) // Enable High
+        HAL.writeBits(DATA_MASK, data)
+        HAL.clearBits(ENABLE_MASK) // Enable Low
 
     }
 
     // Escreve um byte de comando/dados no LCD
     fun writeByte(rs: Boolean, data: Int){
-        val data1 = data
+        writeNibble(rs,data shr 4)
         writeNibble(rs, data)
-        writeNibble(rs, data shr 4)
+        HAL.setBits(ENABLE_MASK)
+        HAL.clearBits(ENABLE_MASK)
+        HAL.setBits(ENABLE_MASK)
+        HAL.clearBits(ENABLE_MASK)
+        HAL.setBits(RS_MASK)
+        writeNibble(rs,data shr 4)
+        writeNibble(rs, data)
+        /*HAL.setBits(ENABLE_MASK)
+        HAL.clearBits(ENABLE_MASK)*/
+
     }
 
     // Escreve um comando no LCD
@@ -35,27 +46,42 @@ object LCD{ // Escreve no LCD usando a interface a 4 bits
 
     // Envia a sequência de iniciação para comunicação a 4 bits.
     fun init(){
-        TODO()
+        Time.sleep(15)
+        writeCMD(3)
+        Time.sleep(5)
+        writeCMD( 3)
+        writeCMD(3)
+        writeCMD(2)
+        writeCMD(2)
+        writeCMD(0b1000)
+        writeCMD(0)
+        writeCMD(8)
+        writeCMD(0)
+        writeCMD(1)
+        writeCMD(0b0110)
+        writeCMD(0b1111)
+
     }
 
     // Escreve um caráter na posição corrente.
     fun write(c: Char){
-        TODO()
+        writeDATA(c.toInt())
     }
 
     // Escreve uma string na posição corrente.
     fun write(text: String){
-        TODO()
+        for(char in text) write(char)
     }
 
     // Envia comando para posicionar cursor (‘line’:0..LINES-1 , ‘column’:0..COLS-1)
     fun cursor(line: Int, column: Int){
-        TODO()
+       // writeCMD()
     }
 
     // Envia comando para limpar o ecrã e posicionar o cursor em (0,0)
     fun clear(){
         writeCMD(0b00000001)
+        cursor(0,0)
     }
 }
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,7 @@ import isel.leic.utils.Time
 
 
 fun main(args: Array<String>) {
-    LCD.writeNibble(true, 0b1010)
-    Time.sleep(4000)
-    LCD.writeByte(true, 0b11001001)
+    LCD.init()
+    /*Time.sleep(4000)
+    LCD.init(true, 0b11001001)*/
 }
